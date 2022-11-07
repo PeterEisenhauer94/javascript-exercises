1d49290b952710a33c46deb268d409e73200b1a4
Peter Eisenhauer <petereisenhauer94@gmail.com>
2022-11-07 18:15:34 -0500

    resolve tests for exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let returnedString = '';
+    for (i=1;i<=string.length;i++){
+        returnedString = returnedString + string[string.length-i];
+    }
+    return returnedString;
 };
 
 // Do not edit below this line
