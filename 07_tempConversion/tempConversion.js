0cbb3709f9aa9a48aa8e064e2e563a35b78173d7
Peter Eisenhauer <petereisenhauer94@gmail.com>
2022-11-11 17:06:13 -0500

    finish problem set 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  let result = (num - 32) * .5556;
+  if (result%1===0){
+    return result;
+  } else {
+    return +result.toFixed(1);
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  let result = (num*1.8) + 32;
+  if (result%1===0){
+    return result;
+  } else {
+    return +result.toFixed(1);
+  }
 };
 
 // Do not edit below this line
