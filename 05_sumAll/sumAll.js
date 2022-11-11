b4f782c1bffbc0b751b9d3502ce0de74145c320c
Peter Eisenhauer <petereisenhauer94@gmail.com>
2022-11-11 15:57:15 -0500

    solve problem set five

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(int1,int2) {
+    let result = 0;
+    let floor = 0;
+    let ceiling = 0;
 
+    //verify that arguments are positive integers
+    if ((int1<0||int2<0)||typeof(int1,int2)!='number'){
+        return "ERROR";
+    }
+
+    //determine starting number
+    if (int1 < int2){
+        floor = int1;
+        ceiling = int2;
+    } else {
+        floor = int2;
+        ceiling = int1;
+    }
+
+    //sum all numbers between floor and ceiling, inclusive
+    for(let i = floor; i <= ceiling; i++){
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
