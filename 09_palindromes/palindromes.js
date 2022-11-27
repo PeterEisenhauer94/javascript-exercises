74715fb46fae9f6e999ad7dc30595a3cd6f10cda
Peter Eisenhauer <petereisenhauer94@gmail.com>
2022-11-27 10:57:46 -0500

    solve problem set 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,30 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    
+    //clean the string
+    string = string.replace(/[.,\/#!$%\^&\*;:{}\s=\-_`~()]/g,"");
+    console.log(string);
+    string = string.toLowerCase();
+    console.log(string)
 
+    //convert string to array
+    let ptest = string.split("");
+    let result= [];
+    for (i in string){
+        result.push(ptest.pop());
+    }
+    //test if palindrome
+    
+    let test = function(string,result){
+        for (let i=0; i<string.length-1;i++){
+            if (string[i]!==result[i])
+            return false;
+        }
+        return true;
+    }
+    test(string,result);
+
+    //return result
+   return test(string,result);
 };
-
 // Do not edit below this line
 module.exports = palindromes;
